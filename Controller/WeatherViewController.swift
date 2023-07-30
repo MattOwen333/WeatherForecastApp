40ff51e6bb1f6a832105c667b7a1836a701b36b8
Matthew Owen <MattOwen333@gmail.com>
2023-07-30 18:29:34 -0400

    changes working towards using task

--- Controller/WeatherViewController.swift
+++ Controller/WeatherViewController.swift
@@ -8,23 +8,23 @@
 import UIKit
 
 class WeatherViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
-
-
+    
+    
     
     @IBOutlet weak var tableView: UITableView!
     var weatherData = WeatherData()
-
+    
     func configureTable() {
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     
     var city: String = "Atlanta"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         
         Task {
             do {
@@ -39,53 +39,59 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, UITableViewD
         }        // self refers to the current ViewController
         weatherData.loadDataSource()
         configureTable()
-      
+        
     }
     
-     
-        
-        
-        
-    private func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) async
-        
-        
-        
-        {
-            let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-            
-            print(indexPath.row)
-            
-            navigationController?.pushViewController(detailVC, animated: true)
-            
-                    let citySelction = try? await weatherData.fetchWeatherData(city: citiesList[indexPath.row])
-            
-            print(citySelction)
-            
-            
-        }
     
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 100
-        }
-        //Mark: UITableiewDataSource
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return citiesCount  }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CustomCell
-
-            cell.cellLable.text = citiesList[indexPath.row]
-
-            return cell
-
-        }
-
-
-
-      
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    
+    
+    
+    {
+        let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        
+        print(indexPath.row)
+        
+        navigationController?.pushViewController(detailVC, animated: true)
+        
+//        let citySelction = try? weatherData.fetchWeatherData(city: citiesList[indexPath.row])
+//        print(citySelction)
+        
+        
+        print(indexPath.row)
+        
+        
+        
     }
     
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+    
+    //Mark: UITableiewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citiesCount  }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CustomCell
+        
+        cell.cellLable.text = citiesList[indexPath.row]
+        
+        return cell
+        
+    }
+}
+
+
+
+
     
   
     
