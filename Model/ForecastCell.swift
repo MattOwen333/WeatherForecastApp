ce3c332da6593c8e1fedbc0050dae07e6016abe9
Matthew Owen <MattOwen333@gmail.com>
2024-03-11 07:00:27 -0400

    Made some code Optimizations

--- Model/ForecastCell.swift
+++ Model/ForecastCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class ForecastCell : UITableViewCell {
     
     @IBOutlet weak var ForecastCellLabelWind: UILabel!
@@ -33,3 +34,50 @@ class ForecastCell : UITableViewCell {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============================================
+
+//class ForecastCell : UITableViewCell {
+//
+//    @IBOutlet weak var ForecastCellLabelWind: UILabel!
+//    @IBOutlet weak var ForecastingCellLabelTemp: UILabel!
+//    @IBOutlet weak var ForecastingCellLabelDate: UILabel!
+//
+//    func configureForecastUI(forecast: Forecast, index: Int) {
+//        ForecastingCellLabelTemp.text = forecast.temperature
+//        ForecastCellLabelWind.text = forecast.wind
+//
+//        let currentDate = Date()
+//        var calendar = Calendar.current
+//        var dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "MM/dd/yyyy"
+//
+//        // Get the current date
+//        var forecastDate = currentDate
+//
+//        // Calculate the date for the first forecast
+//        if index == 0 {
+//            let todayDateString = dateFormatter.string(from: currentDate)
+//            ForecastingCellLabelDate.text = todayDateString
+//        } else {
+//            // Calculate subsequent dates for forecasts
+//            forecastDate = calendar.date(byAdding: .day, value: index, to: currentDate)!
+//            let forecastDateString = dateFormatter.string(from: forecastDate)
+//            ForecastingCellLabelDate.text = forecastDateString
+//        }
+//    }
+//}
